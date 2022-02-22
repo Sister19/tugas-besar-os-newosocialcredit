33091960d72e0ac5515725731b9de6ca8ff9ed54
clumsyyyy <owencwijaya@gmail.com>
2022-02-22 19:09:06 +0700

    clearScreen added, stdlib not yet defined

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -1,31 +1,38 @@
-// Kode kernel
-// PENTING : FUNGSI PERTAMA YANG DIDEFINISIKAN ADALAH main(),
-//   cek spesifikasi untuk informasi lebih lanjut
-
-// TODO : Tambahkan implementasi kode C
-
+// PROTOTYPES
 #define true 1
 #define false 0
 #define length 128
 #define endl printString("\r\n");
-#include "header/kernel.h"
-#include "header/std_type.h"
+
+void printString(char* buffer);
+void readString(char* buffer);
+void clearScreen();
+void printChar(char* c);
+int len(char* buffer);
+void printTitle();
 
 
 int main(){
   interrupt(0x10, 0x0003, 0, 0, 0); //set video mode to 03
+  printTitle();
   while (true){
     char buffer[length + 3];
     makeInterrupt21();
     endl;
     printString("Input string (~: clear): ");
     readString(buffer);
-    printString("\r\nYou inputted: ");
-    printString(buffer);
-    printString("\r\n");
-    }  
+    //clear screen if tilde (idk)
+    if (buffer[0] == '~'){
+      clearScreen();
+    }
+    else{
+      printString("\r\nYou inputted: ");
+      printString(buffer);
+      printString("\r\n");
+    }
+    // clearScreen();
+  }
 }
-
 
 void handleInterrupt21(int AX, int BX, int CX, int DX) {
   switch (AX){
@@ -72,4 +79,60 @@ void readString(char* buffer){
       index++;
     }
   }
+}
+
+int len(char* buffer){
+  int count = 0;
+  while (buffer[count] != '\0' && buffer[count + 1] != '\r'){
+    count++; //count string as long as its not null
+  }
+  return count;
+}
+
+void clearScreen(){
+  /*interrupt for scrolling down (clearing screen you can say)
+    16-bits register can be divided into two parts:
+      ...X = |  ...H |  ...L |
+    
+    mode: 0x10 (write)
+    AX, function code register (0x0600)
+      AH = 0x06 (scroll up)
+      AL = 0x00 (scroll up 0 lines, dependent to DX)
+    BX, color register (0x0F00),
+      BH: 0x0F (white color / foreground)
+      BL: 0x00 (black color / background)
+    CX, number of beginning row and column
+      CH: upper row number, 0
+      CL: left column number, 0
+    DX, number of ending row and column
+      DH: lower row number, 24 (0x18)
+      DL: right column number, 79 (0x4F)
+  */
+  interrupt(0x10, 0x0600, 0x0F00, 0x0000, 0x184F);
+
+  /* interrupt to reposition cursor, 0x02 = reposition cursor, 
+  DX = 0 because the position we wanted is 0, 0 */
+
+  interrupt(0x10, 0x0200, 0, 0, 0);
+
+  /* message */
+  printString("Screen cleared!");
+}
+
+void printTitle(){
+  
+  printString(" ________   _______   ___       __   ________  ________      "); endl;
+  printString("|\\   ___  \\|\\  ___ \\ |\\  \\     |\\  \\|\\   __  \\|\\   ____\\     "); endl;
+  printString("\\ \\  \\ \\  \\ \\   __/|\\ \\  \\    \\ \\  \\ \\  \\|\\  \\ \\  \\___|_    "); endl;
+  printString(" \\ \\  \\ \\  \\ \\  \\_|/_\\ \\  \\  __\\ \\  \\ \\  \\\\  \\ \\_____  \\   "); endl;
+  printString("  \\ \\  \\ \\  \\ \\  \\_|\\ \\ \\  \\|\\__\\_\\  \\ \\  \\\\  \\|____|\\  \\  "); endl;
+  printString("   \\ \\__\\ \\__\\ \\_______\\ \\____________\\ \\_______\\____\\_\\  \\ "); endl;
+  printString("    \\|__| \\|__|\\|_______|\\|____________|\\|_______|\\_________\\"); endl;
+  printString("                                                 \\|_________|"); endl;
+  endl;
+  printString("(tentative name because it's still kinda horrendous right now)"); endl;
+  endl; 
+  printString("What you can CURRENTLY do right now, though: "); endl;
+  printString("1. Enter a string and the OS will output it again (yes)"); endl;
+  printString("2. Enter '~' to clear the terminal, sounds quite simple (because it is) "); endl; endl;
 }
