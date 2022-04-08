01c7c26146ef1134d3e524f211d1f1c5385fb19c
Amar Fadil <34912365+marfgold1@users.noreply.github.com>
2022-04-09 03:24:44 +0700

    Almost finished: fix bug cp

---
+++ src/c/apps/cp.c
@@ -0,0 +1,78 @@
+#include "../library/program.h"
+void cpFile(byte from, byte to, char* last_name, char* current_name);
+
+// copy file 1 (path1) to desired path (path2)
+void main(){
+    byte buffer[512 * 16];
+    struct file_metadata metadata;
+    enum fs_retcode ret;
+
+    byte cdir1, ldir1, cdir2, ldir2;
+    char* path1, path2;
+    char name_res[14];
+    // always include these three lines (message passing).
+    struct shell_data data;
+    struct node_entry node;
+    struct node_filesystem node_fs_buffer;
+    getShellData(&data);
+    if (data.cwd.arg_count > 2) {
+        readsNode(&node_fs_buffer);
+        metadata.buffer = buffer;
+        path1 = data.arg.argv[1];
+        path2 = data.arg.argv[2];
+        parsePathArg(path1, data.cwd.current_dir, &cdir1, &ldir1, name_res, &node);
+        // check if path exists
+        // "cat" implementation here, 
+        if (checkIsExist(path1, cdir1)){
+            parsePathArg(path2, data.cwd.current_dir, &cdir2, &ldir2, name_res, &node);
+            // if the second path is not found, use it as rename
+            if (cdir2 == IDX_NODE_UNDEF){
+                // and the second path is directory, copy folder
+                if (checkIsExist(path2, ldir2) && checkIsDirectory(&node, path2, ldir2)){
+                    metadata.parent_index = ldir1;
+                    strncpy(metadata.node_name, node_fs_buffer.nodes[cdir1].name, 14);
+                    read(&metadata, &ret);
+                    metadata.parent_index = ldir2;
+                    strncpy(metadata.node_name, name_res, 14);
+                    write(&metadata, &ret);
+                    switch (ret){
+                        case FS_SUCCESS:
+                            puts("Copy success.\n");
+                            break;
+                        case FS_W_NOT_ENOUGH_STORAGE:
+                            putsc("Error: Not enough storage.\n", COLOR_LIGHT_RED);
+                            break;
+                        default:
+                            puts("Error "); puts(itoa(ret)); endl;
+                            break;
+                    }
+                }
+            } else {
+                if (
+                    isDirectory(&node, cdir2)
+                    && getNodeIdx(&node_fs_buffer, node_fs_buffer.nodes[cdir1].name, cdir2) == IDX_NODE_UNDEF
+                ) {
+                    metadata.parent_index = ldir1;
+                    strncpy(metadata.node_name, node_fs_buffer.nodes[cdir1].name, 14);
+                    read(&metadata, &ret);
+                    metadata.parent_index = cdir2;
+                    write(&metadata, &ret);
+                    switch (ret){
+                        case FS_SUCCESS:
+                            puts("Copy success.\n");
+                            break;
+                        case FS_W_NOT_ENOUGH_STORAGE:
+                            putsc("Error: Not enough storage.\n", COLOR_LIGHT_RED);
+                            break;
+                        default:
+                            puts("Error "); puts(itoa(ret)); endl;
+                            break;
+                    }
+                } else {
+                    putsc("Can't copy file into file.\n", COLOR_LIGHT_RED);
+                }
+            }
+        }
+    }
+    exit(&data);
+}
---
+++ src/c/apps/mv.c
@@ -0,0 +1,46 @@
+#include "../library/program.h"
+
+void main() {
+    byte cdir1, ldir1, arg_cdir, arg_ldir;
+    char name_res[14];
+    char *path1, *path2;
+    struct shell_data data;
+    struct node_filesystem node_fs_buffer;
+    struct node_entry node;
+    getShellData(&data);
+    if (data.cwd.arg_count > 2) {
+        path1 = data.arg.argv[1];
+        path2 = data.arg.argv[2];
+        parsePathArg(path1, data.cwd.current_dir, &cdir1, &ldir1, name_res, &node);
+        readsNode(&node_fs_buffer);
+        if (checkIsExist(path1, cdir1)) { // make sure path1 exist
+            parsePathArg(path2, data.cwd.current_dir, &arg_cdir, &arg_ldir, name_res, &node);
+            if (arg_cdir == IDX_NODE_UNDEF) { // dest is not exist, use it as new name
+                // last dest should exist and is directory
+                if (checkIsExist(path2, arg_ldir) && checkIsDirectory(&node, path2, arg_ldir)) {
+                    // we rename the folder.
+                    strcpy(node_fs_buffer.nodes[cdir1].name, name_res);
+                    // we move it to new parent (arg_ldir)
+                    node_fs_buffer.nodes[cdir1].parent_node_index = arg_ldir;
+                    writesNode(&node_fs_buffer);
+                    puts("Move success.\n");
+                }
+            } else { // dest is exist, we just move the directory.
+                if (
+                    isDirectory(&node, arg_cdir)
+                    && getNodeIdx(&node_fs_buffer, node_fs_buffer.nodes[cdir1].name, arg_cdir) == IDX_NODE_UNDEF
+                ) { 
+                    node_fs_buffer.nodes[cdir1].parent_node_index = arg_cdir;
+                    writesNode(&node_fs_buffer);
+                    puts("Move success.\n");
+                } else { // if dest is a file, we can't move it.
+                    if (isDirectory(&node_fs_buffer.nodes[cdir1], cdir1))
+                        puts("Can't move directory into file.\n");
+                    else
+                        puts("Can't move file into file.\n");
+                }
+            }
+        }
+    }
+    exit(&data);
+}
